8f6ba95d68e084817508987642454d6ed39196c9
M. Andrés Piana <153022165+MateoAPiana@users.noreply.github.com>
2024-10-30 18:23:37 -0300

    initials changes

--- src/const/const.js
+++ src/const/const.js
@@ -55,14 +55,6 @@ export const skills = [
     icon: IconKeys.javaScript,
   },
   {
-    title: "Astro",
-    icon: IconKeys.astro,
-  },
-  {
-    title: "Tailwindcss",
-    icon: IconKeys.tailwind,
-  },
-  {
     title: "React Js",
     icon: IconKeys.react,
   },
@@ -89,10 +81,6 @@ export const skills = [
     icon: IconKeys.redux,
   },
   {
-    title: "Styled Components",
-    icon: IconKeys.styled,
-  },
-  {
     title: "Node Js",
     icon: IconKeys.nodejs,
   },
@@ -100,11 +88,6 @@ export const skills = [
     title: "TypeScript",
     icon: IconKeys.typeScript,
   },
-  {
-    title: "Radix UI",
-    icon: IconKeys.radix,
-  },
-
   {
     title: "React Query",
     icon: IconKeys.reactQuery,
@@ -117,21 +100,12 @@ export const skills = [
     title: "Zustand",
     icon: IconKeys.zustand,
   },
-
-  {
-    title: "Shadcn UI",
-    icon: IconKeys.shadcn,
-  },
-  {
-    title: "Mongo Db",
-    icon: IconKeys.mongodb,
-  },
-];
+]
 
 export const projects = [
   {
-    img: "../../../img/tasks-manager.webp",
-    title: "Tasks Manager",
+    img: "../../../img/chess-project.png",
+    title: "Chess",
     link: "https://task-manager-ecru-chi.vercel.app/dashboard",
     description: `Administrador de tareas con drag and drop autenticación con Clerk , shadcn.  "En Desarrollo "`,
     technologies: [
@@ -139,190 +113,31 @@ export const projects = [
         icon: IconKeys.typeScript,
       },
       {
-        icon: IconKeys.nextjs,
+        icon: IconKeys.vite,
       },
       {
-        icon: IconKeys.tailwind,
+        icon: IconKeys.css,
       },
       {
         icon: IconKeys.zustand,
       },
       {
-        icon: IconKeys.shadcn,
+        icon: IconKeys.nodejs,
       },
     ],
   },
   {
-    img: "../../../img/ecommerce-stripe-2.webp",
-    title: "Ecommerce Stripe",
-    link: "https://ecommerce-stripe-763m.vercel.app/",
-    description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
+    img: "../../../img/Encuesta-enj.png",
+    title: "Encuesta ENJ",
+    link: "https://encuesta-ENJ.mateoapiana.website/",
+    description: `Encuesta organizada por alumnos de la Escuela del Niño Jesús`,
     technologies: [
       {
-        icon: IconKeys.typeScript,
+        icon: IconKeys.javaScript,
       },
-      {
-        icon: IconKeys.nextjs,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.mongodb,
-      },
-      {
-        icon: IconKeys.stripe,
-      },
-    ],
-  },
-  {
-    img: "../../../img/nexanime.webp",
-    title: "NexAnime",
-    link: "https://nexanime.vercel.app",
-    description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
-    technologies: [
-      {
-        icon: IconKeys.typeScript,
-      },
-      {
-        icon: IconKeys.nextjs,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/store-games.webp",
-    title: "Store games",
-    link: "https://store-games-site.netlify.app/",
-    description: `Simulando una página de juegos , consumiendo la API de rawg api
-      donde los usuarios pueden buscar videojuegos, ver sus detalles como
-      creadores , calificaciones,tiendas donde comprar.....`,
-    technologies: [
       {
         icon: IconKeys.react,
       },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.reactQuery,
-      },
     ],
-  },
-  {
-    img: "../../../img/movie.webp",
-    title: "Movie Tv",
-    link: "https://movietvsite.netlify.app/",
-    description: `simulando una página de películas , consumiendo la API de themovietv
-              donde los usuarios pueden buscar películas y ver trailer.....`,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/cyberpunk.webp",
-    title: "cyberpunk",
-    link: "https://preeminent-liger-266356.netlify.app/",
-    description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
-      el ususario puede ver los tailer de él juegos y también los
-      episodios de la serie....`,
-    technologies: [
-      {
-        icon: IconKeys.html,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-    ],
-  },
-
-  {
-    img: "../../../img/fit-nation.webp",
-    title: "gym",
-    link: "https://fitnation.netlify.app/",
-    description: `landing page de una gym por ahora es una landing todavia está en
-              desarrollo la idea es que los usuarios puedan comprar productos de
-              el gym`,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/spotify.webp",
-    title: "Spotify-Clone",
-    link: "https://www.linkedin.com/posts/osnaider-mart%C3%ADnez_clon-de-la-p%C3%A1gina-de-spotify-echo-con-react-activity-7119712602342965248-upgL?utm_source=share&utm_medium=member_desktop",
-    description: `Clon de la página de Spotify con autenticación de usuario, reproductor de canciones, etc `,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.redux,
-      },
-      {
-        icon: IconKeys.reactQuery,
-      },
-    ],
-  },
-
-  {
-    img: "../../../img/landing.webp",
-    title: "Landing Realidad Virtual",
-    link: "https://realityvirtual.netlify.app/",
-    description: `landing page de gafas de realidad virtual donde se muestra el
-              producto y como funciona`,
-    technologies: [
-      {
-        icon: IconKeys.astro,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
+  }
 ];
